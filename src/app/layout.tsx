d6de53c48b0ce8465fb0070164c4b4e68ef3bd9c
Aminul Islam <me@aminul.net>
2024-06-08 22:19:42 +0600

    wip

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Awesomes by Aminul",
+  description: "A curated list of awesome things by Aminul",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
